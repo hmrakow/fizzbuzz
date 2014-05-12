4d0938f22bd85d4df4df70fb5f85dd8e79081fdb
hmrakow <hmrakow@gmail.com>
2014-05-12 18:02:08 -0400

    fizz buzz take 1
    created a tool to count to 100 fizz buzz style

---
+++ script.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+
+for (var i = 1; i < 101; i++) {
+	if (i%3  === 0 ){ 
+		if (i%15 === 0){
+		console.log('fizzbuzz');
+		}
+		else{
+			console.log('fizz');
+			}
+	}
+	else if (i%5 === 0){ 
+	console.log('buzz');
+	}
+	else{
+		console.log(i);
+		}
+}
